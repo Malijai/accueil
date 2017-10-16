a0fe51882723dcd24ae2bcbe898caa827b8f8dad
malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2017-10-15 21:16:22 -0400

    premier commit

---
+++ views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def isManitoba(http_host):
+   return True if 'ntpmb.ca' in http_host else False
+   #return True
+
+def accueil(request):
+    if isManitoba(request.META.get('HTTP_HOST')):
+        return render(request,'logged_out.html')
+    else:
+        return render(request, "index.html")
+
