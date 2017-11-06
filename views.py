ee58c1ede16507cb84a2fd2e4a7a407f6ba4cbb5
malijai <malijai.caulet.ippm@ssss.gouv.qc.ca>
2017-11-06 17:09:20 -0500

    Adaptation de l'accueil pour adresse dans malijai.org

--- views.py
+++ views.py
@@ -7,9 +7,16 @@ def isManitoba(http_host):
    return True if 'ntpmb.ca' in http_host else False
    #return True
 
+def isMalijai(http_host):
+   return True if 'malijai.org' in http_host else False
+#   return True
+
+
 def accueil(request):
     if isManitoba(request.META.get('HTTP_HOST')):
         return render(request,'logged_out.html')
+    elif isMalijai(request.META.get('HTTP_HOST')):
+        return render(request,'indexmali.html')
     else:
         return render(request, "index.html")
 
